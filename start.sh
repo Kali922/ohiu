769411e92097aee809d182b0fcb0f9d4288f292b
Kali922 <mrk278800@gmail.com>
2024-12-29 19:41:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-# Start aria2c as a daemon
-aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon
-
-# Run terabox.py in the background
-python3 terabox.py &
-
-# Run web.py in the foreground
-python3 web.py
+aria2c --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon && python3 terabox.py && python3 web.py
